a1ed552f1cdb27b8b679a1e6d1f3113a3f88d3a7
ManshajD <72268398+Manshajd@users.noreply.github.com>
2026-05-26 14:42:04 +0530

    step-6 with the sign in route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const zod = require("zod");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../config");
 const router = express.Router();
 const app = express();
 
@@ -10,9 +12,35 @@ const signupSchema = zod.object({
     lastName: zod.string()
 })
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
     const body = req.body;
-    const { success } = signupSchema.safeParse
+    const { success } = signupSchema.safeParse(req.body);
+    if(!success)
+    {
+        return res.json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const user = User.findOne({
+        username: body.username
+    })
+
+    if(user._id)
+    {
+        return res.json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const dbuser = await User.create(body);
+    const token = jwt.sign({
+        userId: dbuser._id
+    }, JWT_SECRET);
+    res.json({
+        message: "user created successfully",
+        token: token
+    })
 })
 
 module.exports = router;
