1f2624d2e8020dc734d738161b882316f707e38d
ManshajD <72268398+Manshajd@users.noreply.github.com>
2026-05-26 15:07:31 +0530

    step-6 added sigin route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -12,6 +12,15 @@ const signupSchema = zod.object({
     lastName: zod.string()
 })
 
+const signinSchema = zod.object({
+    username: z.string(),
+    password: z.string()
+})
+
+router.get("/test", (req,res)=>{
+    res.send("hello world!")
+})
+
 router.post("/signup", async (req, res) => {
     const body = req.body;
     const { success } = signupSchema.safeParse(req.body);
@@ -41,6 +50,44 @@ router.post("/signup", async (req, res) => {
         message: "user created successfully",
         token: token
     })
+})
+
+router.post("/signin", (req, res) => {
+    const succees = siginInput.safeParse(req.body)
+    if(!succees){
+        return res.status(400).json({
+            msg: "Invalid email or password"
+        })
+    }
+
+    const username = user.username
+    const firstName = user.firstName
+
+    const user = await User.findOne({
+            username: req.body.username,
+          password: req.body.password
+        })
+
+
+    if(!user){
+        return  res.status(411).json({
+            msg: "Error while logging in"
+        })
+    }
+    const userId = user._id;
+
+
+    const token = jwt.sign({
+        userId,
+        username,
+        firstName
+    },JWT_Secret, {expiresIn: 86400})
+
+    res.json({
+        msg: "login Succesfull",
+        token
+    })
+
 })
 
 module.exports = router;
