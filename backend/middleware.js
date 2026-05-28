8ed379383f024b0039519cd1e1d79266094a226a
ManshajD <72268398+Manshajd@users.noreply.github.com>
2026-05-28 16:06:31 +0530

    updated in middleware

--- backend/middleware.js
+++ backend/middleware.js
@@ -12,9 +12,16 @@ const authMiddleware = (req, res, next) => {
 
     try{
         const decoded = jwt.verify(token, JWT_SECRET);
-        req.userId = decoded.userId;
+        if(decoded.userId)
+        {
+            req.userId = decoded.userId;
+            next();
+        }
+        else
+        {
+            return res.status(403).json({});
+        }
 
-        next();
     }catch(err){
         return res.status(403).json({});
     }
