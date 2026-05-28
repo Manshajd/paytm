d8228ea19e12cab3be78bdd2247cd04b557d0224
ManshajD <72268398+Manshajd@users.noreply.github.com>
2026-05-28 17:15:16 +0530

    updated the db.js for account Schema

--- backend/db.js
+++ backend/db.js
@@ -13,15 +13,24 @@ const userSchema = new Schema({
     id: String,
     firstName: {
         type:String,
-        required: true
+        required: true,
+        trim: true,
+        maxLength: 50
     },
     lastName: {
         type:String,
-        required: true
+        required: true,
+        trim: true,
+        maxLength: 50
     },
     username: {
         type:String,
-        required: true
+        required: true,
+        unique: true,
+        trim: true,
+        lowerCase: true,
+        minLength: 3,
+        maxLength: 30
     },
     password: {
         type:String,
@@ -32,7 +41,24 @@ const userSchema = new Schema({
     timestamps:true 
 });
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+
 const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accountSchema);
 
 // export default User;
-module.exports = { User };
+module.exports = { 
+    User,
+    Account 
+};
